55951aab35e7472b104d58b001530171752c86e1
Chase Van Halen <cvanhalen@perscholas.org>
2023-04-18 12:06:16 -0500

    THIS BREAKS THE APPLICATION

--- index.js
+++ index.js
@@ -24,3 +24,22 @@ form.addEventListener('submit', (e) => {
 const addTodo = () => {
     
 }
+
+
+
+
+
+// 4 ways to select elements using JS
+
+// 1 getElementById
+const navbar = document.getElementById('my-nav');
+
+// 2 getElementsByClassName
+const arrayOfNavbarButtons = document.getElementsByClassName('navbar-button')
+
+// 3 querySelector (selects first element found that matches the css selector)
+// uses css selector style
+const firstNavbarButton = document.querySelector('nav .navbar-button');
+
+// 4 querySelectorAll (selects all elements found that match css selector)
+const allNavbarButtons = document.querySelector('nav .navbar-button');
